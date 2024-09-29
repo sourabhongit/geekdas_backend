84c859a70aafee14bd3f6de82735daecc6e67779
sourabhongit <get.sourabhdas@gmail.com>
2024-09-29 12:26:37 +0530

    Added comments and auth route

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
 const app = express();
 const path = require('path');
+
+// View enginea
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+// Static assets
 app.use(express.static(path.join(__dirname, 'Public')));
 app.use(cors({ origin: "http://localhost:3000" }));
+
+// Set views
 app.set('views', path.join(__dirname, './Views'));
 
 // Routes
 app.use('/admin', require('./Routes/AdminRoutes/AdminRoutes'));
+app.use('/admin/auth', require('./Routes/AuthRoutes/AuthRoutes'));
 
 let port = process.env.PORT;
 if (port == null || port == "") {
