d0f210af79011b399793734c7d59dc9ee400defd
sourabhongit <get.sourabhdas@gmail.com>
2024-10-05 21:47:02 +0530

    Added express endpoint and updated cors url

--- app.js
+++ app.js
@@ -1,46 +1,36 @@
-require('dotenv').config();
+require("dotenv").config();
 const express = require("express");
-const session = require("express-session");
 const cors = require("cors");
-const ConnectDB = require('./Config/database');
+const ConnectDB = require("./Config/database");
 const bodyParser = require("body-parser");
 const app = express();
 const path = require("path");
+const ListEndpoints = require("express-list-endpoints");
 
-// View enginea
+// View engine setup
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// Set up session management
-app.use(
-	session({
-		secret: "yourSecretKey",
-		resave: false,
-		saveUninitialized: true,
-		cookie: { secure: false }, // Note: Set secure to true in production with HTTPS
-	})
-);
-
 // Static assets
 app.use(express.static(path.join(__dirname, "Public")));
-app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors({ origin: process.env.CLIENT_URL || "http://localhost:3000" }));
 
 // Set views
 app.set("views", path.join(__dirname, "./Views"));
 
-// Database
+// Database connection
 ConnectDB();
 
 // Routes
 app.use("/admin", require("./Routes/AdminRoutes/AdminRoutes"));
 app.use("/admin/auth", require("./Routes/AuthRoutes/AuthRoutes"));
 
-let port = process.env.PORT;
-if (port == null || port == "") {
-	port = 3001;
-}
+// List all the routes in console
+console.log(ListEndpoints(app));
+
+let port = process.env.PORT || 3000;
 
 app.listen(port, function () {
-	console.log("Server started successfully on", { port });
+	console.log(`Server started successfully on port ${port}`);
 });
