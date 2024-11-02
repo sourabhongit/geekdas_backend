268ec77b62f155ce8f91f982ac9754d96795f7d0
sourabhongit <get.sourabhdas@gmail.com>
2024-11-03 00:08:39 +0530

    git commit -m added action on login form added logout functionallity and session store

--- Controller/Auth/AuthController.js
+++ Controller/Auth/AuthController.js
@@ -1,17 +1,38 @@
-const User = require('../../Models/Auth/UserModel');
+const bcrypt = require("bcrypt");
+const User = require("../../Models/Auth/UserModel");
 
 exports.Login = (req, res) => {
-	console.log(req);
 	res.render("Auth/Login");
 };
 
-exports.LoginStore = (req, res) => {
+exports.LoginStore = async (req, res) => {
 	const { username, password } = req.body;
-	const user = User.findUser(username);
+	try {
+		const user = await User.findOne({ username: username });
+		if (user) {
+			const isMatch = await bcrypt.compare(password, user.password);
 
-	if (user && user.password === password) {
-		 res.redirect('/admin/dashboard');
-	} else {
-		 res.render("Auth/Login", { error: 'Invalid username or password' });
+			if (isMatch) {
+				req.session.userId = user._id;
+				// Redirect to dashboard upon successful login
+				return res.redirect("/admin/dashboard");
+			}
+		}
+
+		// If user not found or password does not match, show an error
+		res.render("Auth/Login", { error: "Invalid username or password" });
+	} catch (error) {
+		console.error("Error during login:", error);
+		res.render("Auth/Login", { error: "An error occurred. Please try again." });
 	}
+};
+
+exports.Logout = async (req, res) => {
+	req.session.destroy((err) => {
+		if (err) {
+			 console.error("Session destruction error:", err);
+			 return res.status(500).send('Could not log out. Please try again later.');
+		}
+		res.redirect('/admin/auth/login');
+  });
 };
